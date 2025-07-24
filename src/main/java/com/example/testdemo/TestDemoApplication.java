33e697afd537b9dc010da1ebdc4b1df5d159195b
KokoYu <yukaiyuan99@foxmail.com>
2025-07-24 18:38:34 +0800

    01_commit

---
+++ src/main/java/com/example/testdemo/TestDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.testdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/* 启动类 */
+@SpringBootApplication
+public class TestDemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("启动SpringBoot程序");
+        SpringApplication.run(TestDemoApplication.class, args);
+    }
+
+}
