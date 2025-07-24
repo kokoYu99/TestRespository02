67266a194643ca07acbbc871c765c799a3911eaf
KokoYu <yukaiyuan99@foxmail.com>
2025-07-24 19:40:40 +0800

    branch02_commit

--- src/main/java/com/example/testdemo/BlaBla.java
+++ src/main/java/com/example/testdemo/BlaBla.java
@@ -3,5 +3,6 @@ package com.example.testdemo;
 public class BlaBla {
     public static void main(String[] args) {
         System.out.println("branch01修改内容");
+        System.out.println("branch02修改内容");
     }
 }
